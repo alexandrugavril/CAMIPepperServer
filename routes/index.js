d1c4447af3f72343754c54efe9343495c2488680
Alex Awada <awadaalex@hotmail.com>
2017-11-06 18:32:37 +0200

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -39,6 +39,37 @@ router.get('/HeartRate',function(req, res, next) {
 
 });
 
+router.get('/WebHeartRate',function(req, res, next) {
+    var options = {
+        host: '141.85.241.224',
+        port: 8008,
+        path: '/api/v1/measurement/?measurement_type=pulse&order_by=-timestamp&user=2'
+    };
+    var req = http.get(options, function(response) {
+        console.log('STATUS: ' + response.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        // Buffer the body entirely for processing as a whole.
+        var bodyChunks = [];
+        response.on('data', function(chunk) {
+            // You can process streamed parts here...
+            bodyChunks.push(chunk);
+        }).on('end', function() {
+            var body = Buffer.concat(bodyChunks);
+
+            var reqBody = JSON.parse(body);
+            var hrData = reqBody.measurements;
+            console.log(hrData);
+            res.render('HeartRate', { title: '', plotData:hrData});
+        })
+    });
+
+    req.on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+    });
+
+
+});
+
 router.get('/Weight',function(req, res, next) {
     var options = {
         host: '141.85.241.224',
@@ -70,6 +101,37 @@ router.get('/Weight',function(req, res, next) {
 
 });
 
+router.get('/WebWeight',function(req, res, next) {
+    var options = {
+        host: '141.85.241.224',
+        port: 8008,
+        path: '/api/v1/measurement/?measurement_type=weight&order_by=-timestamp&user=2'
+    };
+    var req = http.get(options, function(response) {
+        console.log('STATUS: ' + response.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        // Buffer the body entirely for processing as a whole.
+        var bodyChunks = [];
+        response.on('data', function(chunk) {
+            // You can process streamed parts here...
+            bodyChunks.push(chunk);
+        }).on('end', function() {
+            var body = Buffer.concat(bodyChunks);
+
+            var reqBody = JSON.parse(body);
+            var wData = reqBody.measurements;
+            console.log(wData);
+            res.render('Weight', { title: '' , plotData:wData});
+        })
+    });
+
+    req.on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+    });
+
+
+});
+
 router.get('/BloodPressure',function(req, res, next) {
     var options = {
         host: '141.85.241.224',
@@ -91,6 +153,37 @@ router.get('/BloodPressure',function(req, res, next) {
             var bpData = reqBody.measurements;
             console.log(bpData);
             res.render('BloodPressure', { title: 'Blood Pressure' , plotData:bpData});
+        })
+    });
+
+    req.on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+    });
+
+
+});
+
+router.get('/WebBloodPressure',function(req, res, next) {
+    var options = {
+        host: '141.85.241.224',
+        port: 8008,
+        path: '/api/v1/measurement/?measurement_type=blood_pressure&order_by=-timestamp&user=2'
+    };
+    var req = http.get(options, function(response) {
+        console.log('STATUS: ' + response.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        // Buffer the body entirely for processing as a whole.
+        var bodyChunks = [];
+        response.on('data', function(chunk) {
+            // You can process streamed parts here...
+            bodyChunks.push(chunk);
+        }).on('end', function() {
+            var body = Buffer.concat(bodyChunks);
+
+            var reqBody = JSON.parse(body);
+            var bpData = reqBody.measurements;
+            console.log(bpData);
+            res.render('BloodPressure', { title: '' , plotData:bpData});
         })
     });
 
@@ -190,6 +283,38 @@ router.get('/Reminders',function(req, res, next) {
 
             console.log(reqBody);
             res.render('Reminders', { title: 'Reminders', reminders:rems});
+        })
+    });
+
+    req.on('error', function(e) {
+        console.log('ERROR: ' + e.message);
+    });
+
+
+});
+
+router.get('/WebReminders',function(req, res, next) {
+    var options = {
+        host: '141.85.241.224',
+        port: 8008,
+        path: '/api/v1/journal_entries/?user=2&acknowledged=none'
+    };
+    var req = http.get(options, function(response) {
+        console.log('STATUS: ' + response.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        // Buffer the body entirely for processing as a whole.
+        var bodyChunks = [];
+        response.on('data', function(chunk) {
+            // You can process streamed parts here...
+            bodyChunks.push(chunk);
+        }).on('end', function() {
+            var body = Buffer.concat(bodyChunks);
+
+            var reqBody = JSON.parse(body);
+            var rems = reqBody.objects;
+
+            console.log(reqBody);
+            res.render('Reminders', { title: '', reminders:rems});
         })
     });
 
